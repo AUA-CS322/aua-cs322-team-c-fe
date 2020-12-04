a2054c5f44b68cc88dd46e249601c368851c3c9b
naroyerevan <narekkarimyan@gmail.com>
2020-12-04 15:14:33 +0400

    index-pagee

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="screen">
     <v-row class="loginCardContainer">
+      <v-col cols="12">
+        <v-banner
+          v-model="showBanner"
+          :sticky="true"
+          single-line
+          icon="error"
+          color="white"
+          icon-color="error"
+          elevation="2"
+        >
+          {{ error && error.message }}
+        </v-banner>
+      </v-col>
+      <v-col>
+        <v-card class="customCard loginCard">
+          <form @submit.prevent="onLogin">
+            <v-row>
+              <v-col cols="12">
+                <h2>Login to platform</h2>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  name="Username"
+                  outlined
+                  required
+                  type="text"
+                  hide-details
+                  label="Username"
+                  v-model="username"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  name="Password"
+                  outlined
+                  hide-details
+                  required
+                  type="password"
+                  label="Password"
+                  v-model="password"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" class="text-right">
+                <v-btn :loading="buttonLoading" type="submit" depressed class="px-8 black white--text">Login</v-btn>
+              </v-col>
+            </v-row>
+          </form>
+        </v-card>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -12,13 +61,41 @@ export default {
       title: `Login`
     }
   },
+  // MIDDLEWATE CHECKING IF THERE IS A LOGGED IN USER REDIRECTS TO /PROFILE PAGE
+  middleware: 'loginCheck',
   data () {
     return {
+      username: '',
+      password: '',
+      buttonLoading: false,
+      showBanner: false,
+      error: null
     }
   },
+  // BEFOREMOUNT WORKS BEFORE THE PAGE IS MOUNTED
   beforeMount () {
+    // SETTING PAGE TITLE
+    this.$store.dispatch('setPageTitle', 'Login Page')
   },
   methods: {
+    onLogin () {
+      this.buttonLoading = true
+      this.showBanner = false
+      this.error = null
+      const formData = new FormData()
+      formData.append('username', this.username)
+      formData.append('password', this.password)
+      this.$store.dispatch('onLogin', formData)
+        .then(() => {
+          this.buttonLoading = false
+          this.$router.push('/profile')
+        })
+        .catch(error => {
+          this.showBanner = true
+          this.error = error
+          this.buttonLoading = false
+        })
+    }
   }
 }
 </script>
